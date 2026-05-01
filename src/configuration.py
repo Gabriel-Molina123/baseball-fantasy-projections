d2b7fddc9049042566804f335812f253f3eee2b0
Gabriel A Molina Ramirez <molinagabriel0509@gmail.com>
2026-05-01 12:18:15 -0400

    Config file (batting)
    Points from stats vary between fantasy leagues

---
+++ src/configuration.py
@@ -0,0 +1,12 @@
+
+SCORING = {
+    "single": 1,
+    "double": 2,
+    "triple:": 3,
+    "home_run": 4, #+RBI + run = 6
+    "walk": 1,
+    "rbi": 1,
+    "run": 1,
+    "stolen_base": 1,
+    "strikeout": -1,
+}
